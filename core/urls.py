d84aa97ee09f1b974dde1f6fab4e590e7405b9d0
MartinEspinozaS <martinlalo10@gmail.com>
2018-11-25 00:16:31 -0700

    ya quedaron todas las vistas de sinlogearse

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
+    path('lista_servicios/',views.lista_servicios, name='lista_servicios'),
+    path('infoservice/', views.infoservice, name='infoservice'),
+    path('lista_campus/', views.lista_campus, name='lista_campus'),
+    path('lista_departamento/', views.lista_departamento, name='lista_departamento'),
 ]
--- core/views.py
+++ core/views.py
@@ -5,3 +5,16 @@ def home_page(request):
     images = Image.objects.order_by('?').first()
     return render(request, 'core/home_page.html', {'images' : images})
     
+def lista_servicios(request):
+    return render(request, 'core/lista_servicios.html' )
+
+def infoservice(request):
+	return render(request, 'core/infoservice.html')
+
+def lista_campus(request):
+	return render(request, 'core/lista_campus.html')
+
+def lista_departamento(request):
+	return render(request, 'core/lista_departamento.html')
+
+   
